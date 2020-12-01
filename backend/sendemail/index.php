bf9e592826dcdfb2c4142a5eb467dd6e281d22df
raj <royaji77@gmail.com>
2020-12-01 10:48:14 +0200

    Update index.php
    Replace private mail with generic one.

--- backend/sendemail/index.php
+++ backend/sendemail/index.php
@@ -23,30 +23,30 @@ if ($_SERVER['HTTP_REFERER'] === "http://localhost:3000/" || "http://localhost:8
     $utmSource = isset($_POST['utm_source']) ? $_POST['utm_source'] : null;
     $utmMedium = isset($_POST['utm_medium']) ? $_POST['utm_medium'] : null;
     $ref = isset($_POST['ref']) ? $_POST['ref'] : null;
-    var_dump($_POST);
+
     require 'vendor/autoload.php';
     $mail = new PHPMailer(true);
     $mail->CharSet = 'UTF-8';
-    echo $ref;
+
     try {
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
         $mail->isSMTP(); // Send using SMTP
         $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
         $mail->SMTPAuth = true; // Enable SMTP authentication
-        $mail->Username = 'testing.ajix@gmail.com'; // SMTP username
-        $mail->Password = 'testing@123'; // SMTP password
+        $mail->Username = 'mailer@example.com'; // SMTP username
+        $mail->Password = 'mailer@123'; // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption;`PHPMailer::ENCRYPTION_SMTP` encouraged
         $mail->Port = 587; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTP` above
 
         //Recipients
-        $mail->setFrom('testing.ajix@gmail.com', 'React Form');
-        $mail->addAddress('testing.ajix@gmail.com'); // Recipient Address('mail', 'name') Name is optional
-        $mail->addReplyTo('testing.ajix@gmail.com', 'Information');
+        $mail->setFrom('sender@example.com', 'React Form');
+        $mail->addAddress('reciever@example.com'); // Recipient Address('mail', 'name') Name is optional
+        $mail->addReplyTo('test@example.com', 'Information');
 
         // Content
         $mail->isHTML(true); // Set email format to HTML
-        $mail->Subject = 'React Form, From Roy Aji - משימת הכרות';
+        $mail->Subject = 'Subject of the email';
         $mail->Body = "<p dir='rtl'> שם פרטי:" . $firstName . "<br/>" .
             "שם משפחה " . $lastName . "<br/>" .
             "נייד " . $phone . "<br/>" .
